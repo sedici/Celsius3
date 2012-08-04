ea7a76917646b1eacf80b2d537d0f6da636853d7
Agustín Marisi <agustinmarisi@gmail.com>
2012-08-04 15:36:29 +0000

    #1927 Modificaciones en los fixtures

--- src/Celsius/Celsius3Bundle/DataFixtures/MongoDB/FixtureLoader.php
+++ src/Celsius/Celsius3Bundle/DataFixtures/MongoDB/FixtureLoader.php
@@ -5,6 +5,7 @@ namespace Celsius\Celsius3Bundle\DataFixtures\MongoDB;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Celsius\Celsius3Bundle\Document;
+use Celsius\Celsius3Bundle\Helper\LoremIpsumHelper;
 
 /**
  * Description of FixtureLoader
@@ -19,15 +20,20 @@ class FixtureLoader implements FixtureInterface
         $names = array('John', 'Tom', 'Laura', 'Mike', 'Chris', 'Michelle', 'Liz', 'Mary', 'Joe', 'Kevin');
         $surnames = array('Perez', 'Rodriguez', 'Dominguez', 'Martinez', 'Gonzalez', 'Ibañez', 'Fernandez', 'Ledesma', 'Acuña', 'Estevez');
 
+        $generator = new LoremIpsumHelper();
+        
+        $min = strtotime('1940-01-01');
+        $max = strtotime('2000-12-31');
+
         for ($i = 0; $i < 5; $i++)
         {
             $instance = new Document\Instance();
-            $instance->setName(md5(rand(0, 99999999)));
-            $instance->setAbbreviation(md5(rand(0, 99999999)));
-            $instance->setTitle(md5(rand(0, 99999999)));
+            $instance->setName(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+            $instance->setAbbreviation(str_replace('. ', '', $generator->getContent(1, 'plain', false)));
+            $instance->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
             $instance->setWebsite(md5(rand(0, 99999999)) . '.test.com');
             $instance->setEmail(md5(rand(0, 99999999)) . '@test.com');
-            $instance->setUrl($i);
+            $instance->setUrl(str_replace('. ', '', $i . '_' . $generator->getContent(1, 'plain', false)));
             $manager->persist($instance);
             $manager->flush();
 
@@ -36,11 +42,11 @@ class FixtureLoader implements FixtureInterface
                 $user = new Document\BaseUser();
                 $user->setName($names[rand(0, 99999999) % 10]);
                 $user->setSurname($surnames[rand(0, 99999999) % 10]);
-                $user->setBirthdate(date('Y-m-d'));
-                $user->setUsername(md5(rand(0, 99999999)));
+                $user->setBirthdate(date('Y-m-d', mt_rand($min,$max)));
+                $user->setUsername($user->getName() . '_' . md5(rand(0, 99999999)));
                 $user->setPassword(md5(rand(0, 99999999)));
                 $user->setEmail($user->getName() . $user->getSurname() . md5(rand(0, 99999999)) . $i . $j . '@test.com');
-                $user->setAddress('address' . md5(rand(0, 99999999)));
+                $user->setAddress('address_' . md5(rand(0, 99999999)));
                 $user->setInstance($instance);
                 $manager->persist($user);
 
@@ -55,8 +61,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -75,8 +81,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -92,11 +98,11 @@ class FixtureLoader implements FixtureInterface
                 $user = new Document\Librarian();
                 $user->setName($names[rand(0, 99999999) % 10]);
                 $user->setSurname($surnames[rand(0, 99999999) % 10]);
-                $user->setBirthdate(date('Y-m-d'));
-                $user->setUsername(md5(rand(0, 99999999)));
+                $user->setBirthdate(date('Y-m-d', mt_rand($min,$max)));
+                $user->setUsername($user->getName() . '_' . md5(rand(0, 99999999)));
                 $user->setPassword(md5(rand(0, 99999999)));
                 $user->setEmail($user->getName() . $user->getSurname() . md5(rand(0, 99999999)) . $i . $j . '@test.com');
-                $user->setAddress('address' . md5(rand(0, 99999999)));
+                $user->setAddress('address_' . md5(rand(0, 99999999)));
                 $user->setInstance($instance);
                 $manager->persist($user);
 
@@ -111,8 +117,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -131,8 +137,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -148,11 +154,11 @@ class FixtureLoader implements FixtureInterface
                 $user = new Document\Admin();
                 $user->setName($names[rand(0, 99999999) % 10]);
                 $user->setSurname($surnames[rand(0, 99999999) % 10]);
-                $user->setBirthdate(date('Y-m-d'));
-                $user->setUsername(md5(rand(0, 99999999)));
+                $user->setBirthdate(date('Y-m-d', mt_rand($min,$max)));
+                $user->setUsername($user->getName() . '_' . md5(rand(0, 99999999)));
                 $user->setPassword(md5(rand(0, 99999999)));
                 $user->setEmail($user->getName() . $user->getSurname() . md5(rand(0, 99999999)) . $i . $j . '@test.com');
-                $user->setAddress('address' . md5(rand(0, 99999999)));
+                $user->setAddress('address_' . md5(rand(0, 99999999)));
                 $user->setInstance($instance);
                 $manager->persist($user);
 
@@ -167,8 +173,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -187,8 +193,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -204,11 +210,11 @@ class FixtureLoader implements FixtureInterface
                 $user = new Document\Superadmin();
                 $user->setName($names[rand(0, 99999999) % 10]);
                 $user->setSurname($surnames[rand(0, 99999999) % 10]);
-                $user->setBirthdate(date('Y-m-d'));
-                $user->setUsername(md5(rand(0, 99999999)));
+                $user->setBirthdate(date('Y-m-d', mt_rand($min,$max)));
+                $user->setUsername($user->getName() . '_' . md5(rand(0, 99999999)));
                 $user->setPassword(md5(rand(0, 99999999)));
                 $user->setEmail($user->getName() . $user->getSurname() . md5(rand(0, 99999999)) . $i . $j . '@test.com');
-                $user->setAddress('address' . md5(rand(0, 99999999)));
+                $user->setAddress('address_' . md5(rand(0, 99999999)));
                 $user->setInstance($instance);
                 $manager->persist($user);
 
@@ -223,8 +229,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -243,8 +249,8 @@ class FixtureLoader implements FixtureInterface
                     $material->setAuthors($names[rand(0, 99999999) % 10] . $surnames[rand(0, 99999999) % 10]);
                     $material->setStartPage(rand(0, 9999999));
                     $material->setEndPage(rand(0, 9999999));
-                    $material->setTitle('title' . md5(rand(0, 9999999)));
-                    $material->setYear(rand(0, 9999999));
+                    $material->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                    $material->setYear(rand(1980, 2012));
 
                     $order->setMaterialData($material);
 
@@ -258,11 +264,9 @@ class FixtureLoader implements FixtureInterface
             for ($j = 0; $j < 100; $j++)
             {
                 $news = new Document\News();
-                $news->setDate(date('Y-m-d H:i:s'));
-                $news->setTitle(md5(rand(0, 9999999)));
-                $news->setText('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacus leo, mattis vitae ullamcorper pulvinar, euismod nec neque. Nulla ultricies, metus eget molestie accumsan, eros orci feugiat orci, non facilisis nisi eros id lectus. Phasellus tempus scelerisque suscipit. Pellentesque quis dolor urna. Proin commodo lectus a erat porttitor tincidunt sit amet at leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst.</p>
-<p>Aliquam sagittis purus eget odio molestie a laoreet nulla pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus consectetur sodales velit, ac cursus diam iaculis vitae. Sed porta quam sed nibh tincidunt aliquam. Ut sit amet dignissim arcu. Integer euismod nulla ac enim mollis eleifend a sed mi. Vivamus porta adipiscing magna vel rhoncus. Aenean viverra, diam eu venenatis semper, nunc mi varius felis, in feugiat enim ipsum ut nulla.</p>
-<p>Nunc tempus venenatis est, ac placerat odio pellentesque id. In nec sapien diam. Mauris lobortis, enim ac accumsan scelerisque, risus libero pulvinar dui, vitae pulvinar erat dolor in felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Sed cursus lectus ut elit adipiscing eu scelerisque erat iaculis. Aliquam non leo vitae mauris vestibulum consequat ut ut nunc. Suspendisse potenti. Nulla id egestas nisl. Duis quam lacus, tincidunt ac pretium at, auctor nec leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce ullamcorper mauris id neque blandit pellentesque. Mauris ultricies congue nulla vitae lobortis. Vestibulum tempor, libero non eleifend auctor, magna velit adipiscing neque, sit amet sodales augue lacus ullamcorper sem. Aliquam at ullamcorper massa.</p>');
+                $news->setDate(date('Y-m-d H:i:s', mt_rand($min,$max)));
+                $news->setTitle(str_replace('. ', '', $generator->getContent(rand(1, 5), 'plain', false)));
+                $news->setText($generator->getContent(rand(100, 500), 'html', false));
                 $news->setInstance($instance);
                 $manager->persist($news);
                 unset($news);
