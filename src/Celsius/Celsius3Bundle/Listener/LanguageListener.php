bf41675529621d02bc4b912d3ab47f22d03ac5e6
Agustín Marisi <agustinmarisi@gmail.com>
2012-09-10 18:14:34 +0000

    #1986 Se reemplazó el aspecto por un listener más similiar al comportamiento estándar de JMSi18nRoutingBundle para evitar conflictos.

--- src/Celsius/Celsius3Bundle/Listener/LanguageListener.php
+++ src/Celsius/Celsius3Bundle/Listener/LanguageListener.php
@@ -2,43 +2,74 @@
 
 namespace Celsius\Celsius3Bundle\Listener;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use FOS\UserBundle\Model\UserInterface;
+use JMS\I18nRoutingBundle\Router\LocaleResolverInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Celsius\Celsius3Bundle\Helper\ConfigurationHelper;
 
 class LanguageListener
 {
 
+    private $defaultLocale;
+    private $locales;
+    private $localeResolver;
     private $container;
     private $dm;
 
-    public function __construct(ContainerInterface $container, DocumentManager $dm)
+    public function __construct($defaultLocale, array $locales, LocaleResolverInterface $localeResolver, ContainerInterface $container, DocumentManager $dm)
     {
+        $this->defaultLocale = $defaultLocale;
+        $this->locales = $locales;
+        $this->localeResolver = $localeResolver;
         $this->container = $container;
         $this->dm = $dm;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        print_r($event->getRequest()->attributes->get('url'));
-//        die();
-//        die();
-//        if (!$this->container->get('request')->attributes->has('_locale'))
-//        {
-//            if ($this->container->get('request')->attributes->has('url'))
-//                $instance_url = $this->container->get('request')->attributes->get('url');
-//            else
-//                $instance_url = $this->container->get('session')->get('instance_url');
-//
-//            $instance = $this->dm
-//                    ->getRepository('CelsiusCelsius3Bundle:Instance')
-//                    ->findOneBy(array('url' => $instance_url));
-//
-//            $locale = $instance->get('instance_language')->getValue();
-//
-//            $this->container->get('request')->attributes->set('_locale', $locale);
-//        }
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType())
+        {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        $ex = $event->getException();
+        if (!$ex instanceof NotFoundHttpException || !$ex->getPrevious() instanceof ResourceNotFoundException)
+        {
+            return;
+        }
+
+        $locale = $this->localeResolver->resolveLocale($request, $this->locales) ? : $this->defaultLocale;
+
+        $path = explode('/', $request->getPathInfo());
+
+        if (!array_key_exists($path[1], ConfigurationHelper::$languages) && !$this->container->get('request')->isXmlHttpRequest())
+        {
+            if ($this->container->get('session')->has('instance_url'))
+                $instance_url = $this->container->get('session')->get('instance_url');
+            else
+                $instance_url = $path[1];
+
+            $instance = $this->dm
+                    ->getRepository('CelsiusCelsius3Bundle:Instance')
+                    ->findOneBy(array('url' => $instance_url));
+
+            if ($instance)
+                $locale = $instance->get('instance_language')->getValue();
+        }
+
+        $request->setLocale($locale);
+
+        $params = $request->query->all();
+        unset($params['hl']);
+
+        $event->setResponse(new RedirectResponse($request->getBaseUrl() . '/' . $locale . $request->getPathInfo() . ($params ? '?' . http_build_query($params) : '')));
     }
 
 }
