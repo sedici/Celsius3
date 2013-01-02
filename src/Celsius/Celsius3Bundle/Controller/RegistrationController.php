a2c42d96fe33e3aa79548817fe6a4f50e98f733b
Agustín Marisi <agustinmarisi@gmail.com>
2013-01-02 17:17:38 +0000

    #2134 Se movieron las acciones que retornan los json de ciudades e instituciones a un controller público.

--- src/Celsius/Celsius3Bundle/Controller/RegistrationController.php
+++ src/Celsius/Celsius3Bundle/Controller/RegistrationController.php
@@ -3,7 +3,6 @@
 namespace Celsius\Celsius3Bundle\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\UserBundle\Controller\RegistrationController as BaseRegistrationController;
 
@@ -59,75 +58,6 @@ class RegistrationController extends BaseRegistrationController
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:waitConfirmation.html.' . $this->getEngine(), array(
                     'user' => $user,
                 ));
-    }
-
-    public function citiesAction()
-    {
-        $request = $this->container->get('request');
-
-        if (!$request->query->has('country_id'))
-        {
-            throw $this->createNotFoundException();
-        }
-
-        $cities = $this->getDocumentManager()->getRepository('CelsiusCelsius3Bundle:City')
-                ->createQueryBuilder()
-                ->field('country.id')->equals($request->query->get('country_id'))
-                ->getQuery()
-                ->execute();
-
-        $response = array();
-
-        foreach ($cities as $city)
-        {
-            $response[] = array('value' => $city->getId(), 'name' => $city->getName());
-        }
-
-        return new Response(json_encode($response));
-    }
-
-    public function institutionsAction()
-    {
-        $request = $this->container->get('request');
-
-        if (!$request->query->has('country_id') && !$request->query->has('city_id') && !$request->query->has('institution_id'))
-        {
-            throw $this->createNotFoundException();
-        }
-
-        $qb = $this->getDocumentManager()->getRepository('CelsiusCelsius3Bundle:Institution')
-                ->createQueryBuilder();
-
-        if ($request->query->has('city_id'))
-        {
-            $qb = $qb->field('city.id')->equals($request->query->get('city_id'))
-                            ->field('parent.id')->equals(null);
-        } else if ($request->query->has('country_id'))
-        {
-            $qb = $qb->field('country.id')->equals($request->query->get('country_id'))
-                            ->field('city.id')->equals(null)
-                            ->field('parent.id')->equals(null);
-        } else
-        {
-            $qb = $qb->field('parent.id')->equals($request->query->get('institution_id'));
-        }
-
-        $institutions = $qb
-                ->getQuery()
-                ->execute();
-
-        $response = array();
-
-        foreach ($institutions as $institution)
-        {
-            $response [] = array(
-                'value' => $institution->getId(),
-                'name' => $institution->getName(),
-                'hasChildren' => $institution->getInstitutions()->count() > 0,
-            );
-        }
-
-        return new Response(json_encode($response));
     }
 
     /**
