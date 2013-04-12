28616a291cc12b1432eaf5c5bb85883f6f9cad0b
José Giraldo <eljosegiraldo@gmail.com>
2013-04-12 14:37:37 +0000

    Se esta trabajando en la forma en que se levantan las notificaciones.

--- src/Celsius/Celsius3Bundle/Controller/UserController.php
+++ src/Celsius/Celsius3Bundle/Controller/UserController.php
@@ -12,6 +12,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class UserController extends BaseInstanceDependentController
 {
+    public function showMessageNotifiaction()
+    {
+        
+     /* $notification = $this->getDocumentManager()
+                             ->getRepository('CelsiusCelsius3Bundle:Notification')
+                             ->createQueryBuilder()
+                             //->field('users.id')->equals($this->getUser()->getId());
+                             ->field('users')
+                             ->includesReferenceTo($this->getUser())
+                             ->field('id')->equals($this->getUser()->getId());
+     */
+        
+   //     $repository = $this->get('doctrine.odm.mongodb.document_manager')
+                            ->getRepository('CelsiusCelsius3Bundle:Notification');
+        
+    //    $notification = $repository->findAll();
+
+        
+        //$notification = $this->listQuery('Notification');
+        
+     
+        
+   //     var_dump($notification->getId());die;
+ 
+
+    }
+    
     /**
      * @Route("/", name="user_index")
      * @Template()
@@ -20,6 +47,8 @@ class UserController extends BaseInstanceDependentController
      */
     public function indexAction()
     {
+      //  $this->showMessageNotifiaction();    
+        
         return array();
     }
 
