fed12bef3d8065edcaed884cd60f6c118bc952f0
Agustín Marisi <agustinmarisi@gmail.com>
2013-04-15 18:09:46 +0000

    Se actualizaron las dependencias para lidiar con un cambio en la librería externa que incluye bootstrap.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,7 +31,7 @@ class AppKernel extends Kernel
             new FOS\MessageBundle\FOSMessageBundle(),
             new Celsius\Celsius3Bundle\CelsiusCelsius3Bundle(),
             new Celsius\Celsius3MessageBundle\CelsiusCelsius3MessageBundle(),
-            new Braincrafted\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test')))
