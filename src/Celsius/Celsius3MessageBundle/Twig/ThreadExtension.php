90d1a7796bf298fcb5b42586111c8586a8c8826e
Agustín Marisi <agustinmarisi@gmail.com>
2013-05-28 19:38:51 +0000

    #2414 Se simplificó el listado de mensajes.

--- src/Celsius/Celsius3MessageBundle/Twig/ThreadExtension.php
+++ src/Celsius/Celsius3MessageBundle/Twig/ThreadExtension.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Celsius\Celsius3MessageBundle\Twig;
-
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use FOS\MessageBundle\Model\ThreadInterface;
 
@@ -24,13 +23,33 @@ class ThreadExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'form_to_thread' => new \Twig_Function_Method($this, 'formToThread'),
-        );
+                'form_to_thread' => new \Twig_Function_Method($this,
+                        'formToThread'),
+                'get_unread_messages' => new \Twig_Function_Method($this,
+                        'getUnreadMessages'),);
     }
 
     public function formToThread(ThreadInterface $thread)
     {
-        return $this->container->get('fos_message.reply_form.factory')->create($thread)->createView();
+        return $this->container->get('fos_message.reply_form.factory')
+                ->create($thread)->createView();
+    }
+
+    public function getUnreadMessages(ThreadInterface $thread)
+    {
+        $participantProvider = $this->container
+                ->get('fos_message.participant_provider');
+
+        $count = 0;
+        foreach ($thread->getMessages() as $message) {
+            if (!$message
+                    ->isReadByParticipant(
+                            $participantProvider->getAuthenticatedParticipant())) {
+                $count++;
+            }
+        }
+
+        return $count;
     }
 
     public function getName()
