c632363181eef5d092bf41e4b492a6d1c7ec8147
Agustín Marisi <agustinmarisi@gmail.com>
2013-05-29 19:28:04 +0000

    #2413 se modificó la vista de un mensaje en particular. Se reemplazaron los textarea normales para la redacción con tinymce.

--- src/Celsius/Celsius3MessageBundle/FormType/NewThreadMultipleMessageFormType.php
+++ src/Celsius/Celsius3MessageBundle/FormType/NewThreadMultipleMessageFormType.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace Celsius\Celsius3MessageBundle\FormType;
-
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Doctrine\ODM\MongoDB\DocumentRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use FOS\MessageBundle\FormType\NewThreadMultipleMessageFormType as BaseNewThreadMultipleMessageFormType;
+use Celsius\Celsius3Bundle\Manager\UserManager;
 
-class NewThreadMultipleMessageFormType extends BaseNewThreadMultipleMessageFormType
+class NewThreadMultipleMessageFormType extends
+        BaseNewThreadMultipleMessageFormType
 {
 
     private $context;
     private $dm;
 
-    public function __construct(SecurityContextInterface $context, DocumentManager $dm)
+    public function __construct(SecurityContextInterface $context,
+            DocumentManager $dm)
     {
         $this->context = $context;
         $this->dm = $dm;
@@ -24,38 +26,40 @@ class NewThreadMultipleMessageFormType extends BaseNewThreadMultipleMessageFormT
     {
         $isAdmin = $this->context->isGranted('ROLE_ADMIN');
         $user = $this->context->getToken()->getUser();
-        if ($isAdmin)
-        {
+        if ($isAdmin) {
             $builder
-                    ->add('recipients', 'recipients_selector_custom', array(
-                        'class' => 'Celsius\\Celsius3Bundle\\Document\\BaseUser',
-                        'property' => 'username',
-                        'multiple' => true,
-                        'query_builder' => function(DocumentRepository $dr) use ($user)
-                        {
-                            return $dr->createQueryBuilder()
-                                    ->field('id')->notEqual($user->getId())
-                                    ->sort('username', 'asc');
-                        },
-            ));
-        } else
-        {
-            $usernames = $this->dm->getRepository('CelsiusCelsius3Bundle:BaseUser')
-                    ->createQueryBuilder()
-                    ->field('id')->notEqual($user->getId())
-                    ->field('instance.id')->equals($user->getInstance()->getId())
-                    ->field('roles')->in(array('ROLE_ADMIN', 'ROLE_SUPER_ADMIN'))
-                    ->getQuery()
+                    ->add('recipients', 'recipients_selector_custom',
+                            array(
+                                    'class' => 'Celsius\\Celsius3Bundle\\Document\\BaseUser',
+                                    'property' => 'username',
+                                    'multiple' => true,
+                                    'query_builder' => function (
+                                            DocumentRepository $dr) use ($user)
+                                    {
+                                        return $dr->createQueryBuilder()
+                                                ->field('id')
+                                                ->notEqual($user->getId())
+                                                ->sort('username', 'asc');
+                                    },));
+        } else {
+            $usernames = $this->dm
+                    ->getRepository('CelsiusCelsius3Bundle:BaseUser')
+                    ->createQueryBuilder()->field('id')
+                    ->notEqual($user->getId())->field('instance.id')
+                    ->equals($user->getInstance()->getId())->field('roles')
+                    ->in(
+                            array(UserManager::ROLE_ADMIN,
+                                    UserManager::ROLE_SUPER_ADMIN))->getQuery()
                     ->execute();
 
-            $builder->add('recipients', 'recipients_selector_hidden', array(
-                'data' => $usernames,
-            ));
+            $builder
+                    ->add('recipients', 'recipients_selector_hidden',
+                            array('data' => $usernames,));
         }
 
-        $builder
-                ->add('subject', 'text')
-                ->add('body', 'textarea');
+        $builder->add('subject', 'text')
+                ->add('body', 'textarea',
+                        array('attr' => array('class' => 'tinymce',),));
     }
 
 }
