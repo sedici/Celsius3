499b3711919212dc9760e49164b38de132faf36c
Agustín Marisi <agustinmarisi@gmail.com>
2013-05-30 18:36:27 +0000

    #2390 se solucionó un error que impedía que se muestren los errores de login.

--- src/Celsius/Celsius3Bundle/Handler/AuthenticationHandler.php
+++ src/Celsius/Celsius3Bundle/Handler/AuthenticationHandler.php
@@ -1,20 +1,28 @@
 <?php
 
 namespace Celsius\Celsius3Bundle\Handler;
-
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\HttpKernel\Log\LoggerInterface;
 
-class AuthenticationHandler implements AuthenticationFailureHandlerInterface, LogoutSuccessHandlerInterface
+class AuthenticationHandler extends DefaultAuthenticationFailureHandler
+        implements AuthenticationFailureHandlerInterface,
+        LogoutSuccessHandlerInterface
 {
-
     private $router;
 
-    public function __construct($router)
+    public function __construct(Router $router, HttpKernelInterface $httpKernel,
+            HttpUtils $httpUtils, array $options,
+            LoggerInterface $logger = null)
     {
+        parent::__construct($httpKernel, $httpUtils, $options, $logger);
         $this->router = $router;
     }
 
@@ -22,14 +30,22 @@ class AuthenticationHandler implements AuthenticationFailureHandlerInterface, Lo
     {
         $url = $request->getSession()->get('instance_url');
 
-        return new RedirectResponse($this->router->generate('public_index', array('url' => $url)));
+        return new RedirectResponse(
+                $this->router->generate('public_index', array('url' => $url)));
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    public function onAuthenticationFailure(Request $request,
+            AuthenticationException $exception)
     {
         $url = $request->attributes->get('url');
-        
-        return new RedirectResponse($this->router->generate('fos_user_security_login', array('url' => $url)));
-    }
 
+        $response = parent::onAuthenticationFailure($request, $exception);
+        $response
+                ->setTargetUrl(
+                        $this->router
+                                ->generate('fos_user_security_login',
+                                        array('url' => $url)));
+
+        return $response;
+    }
 }
