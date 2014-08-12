fc457ca817d8897e66099d5cc6e3a37dcd82d04d
Jorge Agustín Marisi <agustinmarisi@gmail.com>
2014-08-12 20:15:50 -0300

    Se agregó la licencia GPL3

--- src/Celsius3/ApiBundle/DependencyInjection/Celsius3ApiExtension.php
+++ src/Celsius3/ApiBundle/DependencyInjection/Celsius3ApiExtension.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * Celsius3 - Order management
+ * Copyright (C) 2014 PrEBi <info@prebi.unlp.edu.ar>
+ *
+ * This file is part of Celsius3.
+ *
+ * Celsius3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Celsius3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Celsius3.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Celsius3\ApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -14,6 +34,7 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class Celsius3ApiExtension extends Extension
 {
+
     /**
      * {@inheritDoc}
      */
@@ -22,7 +43,7 @@ class Celsius3ApiExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
 }
