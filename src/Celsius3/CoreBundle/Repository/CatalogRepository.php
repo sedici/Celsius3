fc2b031d83ee3bf7b69634d13516f15027ae7c42
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2016-11-24 12:00:06 -0300

    Correción en consulta para listado de catálogos

--- src/Celsius3/CoreBundle/Repository/CatalogRepository.php
+++ src/Celsius3/CoreBundle/Repository/CatalogRepository.php
@@ -37,7 +37,6 @@ class CatalogRepository extends BaseRepository
                         ->where('c.instance = :instance_id')
                         ->andWhere('cp.instance = :instance_id')
                         ->orWhere('c.instance = :directory_id')
-                        ->andWhere('cp.instance = :instance_id')
                         ->orderBy('cp.position', 'asc')
                         ->setParameter('instance_id', $instance->getId())
                         ->setParameter('directory_id', $directory->getId());
