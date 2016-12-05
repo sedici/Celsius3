2768d084fddb36da6eabfc467eb69f0cec921e43
Ariel Sobrado <asobrado@gmail.com>
2016-12-05 11:17:16 -0300

    cambios en el modelo de ticket

--- src/Celsius3/TicketBundle/Controller/TicketController.php
+++ src/Celsius3/TicketBundle/Controller/TicketController.php
@@ -22,7 +22,9 @@ class TicketController extends Controller
      */
     public function indexAction()
     {
-        $tickets = $this->get('celsius3_ticket.ticket_manager')->findAll();
+        $tickets = $this->get('celsius3_ticket.ticket_manager')->findAll(array(), array('updatedAt' => 'ASC'));
+        dump($tickets);
+        die;
         $repository = $this->getDoctrine()->getManager()->getRepository('Celsius3CoreBundle:BaseUser');
         $administradores = $repository->findAdmins($this->get('celsius3_core.instance_helper')->getSessionInstance());
 
