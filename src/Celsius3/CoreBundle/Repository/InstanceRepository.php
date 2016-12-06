bf0442ed0f140df80c8b38a4d61b5142239c66cd
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2016-12-06 15:08:31 -0300

    Se corrigió error en el envío de correos

--- src/Celsius3/CoreBundle/Repository/InstanceRepository.php
+++ src/Celsius3/CoreBundle/Repository/InstanceRepository.php
@@ -60,4 +60,10 @@ class InstanceRepository extends BaseRepository
                     ->execute();
     }
 
+    public function findAllAndInvisibleExceptDirectory()
+    {
+        return $this->createQueryBuilder('i')
+                        ->where('i.url <> :url')
+                        ->setParameter('url', InstanceManager::INSTANCE__DIRECTORY);
+    }
 }
