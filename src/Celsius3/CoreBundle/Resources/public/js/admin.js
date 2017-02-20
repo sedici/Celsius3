1ca239f9723199f08a0f649006c4f1f93c6d7275
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2017-02-20 12:34:26 -0300

    Mejoras para el widget de búsqueda

--- src/Celsius3/CoreBundle/Resources/public/js/admin.js
+++ src/Celsius3/CoreBundle/Resources/public/js/admin.js
@@ -1,15 +1,17 @@
-$('.user-enable').on('click', function (e) {
+$('.user-enable').on('click', function(e) {
     e.preventDefault();
     var item = $(this).parents('.user-item');
     var userId = item.find('input:hidden').val();
     $.ajax({
-        url: Routing.generate('admin_user_enable', {id: userId})
-    }).done(function (data) {
+        url: Routing.generate('admin_user_enable', {
+            id: userId
+        })
+    }).done(function(data) {
         $(item).remove();
     });
 });
 
-$(document).on('click', '.user-enable-modal', function (e) {
+$(document).on('click', '.user-enable-modal', function(e) {
     e.preventDefault();
     var userId = $('.modal-body').children('input:hidden').val();
     var link = $('input:hidden[value="' + userId + '"]').parents('.user-item').find('.user-enable');
@@ -17,14 +19,14 @@ $(document).on('click', '.user-enable-modal', function (e) {
     $('#myModal').modal('hide');
 });
 
-$(function () {
+$(function() {
     $('[data-toggle="popover"]').popover();
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('input[name="disabled-catalogs-view"]').bootstrapSwitch('state', false, true);
 
-    $('input[name="disabled-catalogs-view"]').on('switchChange.bootstrapSwitch', function () {
+    $('input[name="disabled-catalogs-view"]').on('switchChange.bootstrapSwitch', function() {
         if ($(this).is(':checked')) {
             $('.catalog-disabled').css('display', 'block');
         } else {
@@ -32,31 +34,11 @@ $(document).ready(function () {
         }
     });
 
-    $(document).on('click', '.dc-disabled', function (e) {
+    $(document).on('click', '.dc-disabled', function(e) {
         e.preventDefault();
     });
 
-    $(document).on('click', '.disable-double-click', function () {
+    $(document).on('click', '.disable-double-click', function() {
         $(this).addClass('dc-disabled');
     });
-
-    var keyword = $('input#keyword');
-    keyword.closest('form').on('submit', function (e) {
-        keyword.val($.trim(keyword.val()));
-
-        if (keyword.val().length < 3) {
-            e.preventDefault();
-
-            keyword.tooltip({
-                'animation': true,
-                'placement': 'bottom',
-                'title': searchErrorMessage,
-                'trigger': 'hover'
-            });
-            keyword.tooltip('show');
-
-            return;
-        }
-    });
 });
-
