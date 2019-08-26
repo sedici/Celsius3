bc2b5d0a07da4e0fb4b3b68f79dd594ad70e129f
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2019-08-26 18:43:11 -0300

    Correcciónes de configuraciones en desuso

--- web/app.php
+++ web/app.php
@@ -10,11 +10,11 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 $kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
+Request::setTrustedProxies(['127.0.0.1', '10.0.0.1']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
