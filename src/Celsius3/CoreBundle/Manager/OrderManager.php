d212a6d2bd41366dffe5dd0204b94673b86257bd
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2020-09-26 19:23:50 -0300

    Refactor sobre la clase OrderManager

--- src/Celsius3/CoreBundle/Manager/OrderManager.php
+++ src/Celsius3/CoreBundle/Manager/OrderManager.php
@@ -20,30 +20,32 @@
  * along with Celsius3.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 namespace Celsius3\CoreBundle\Manager;
 
-use Celsius3\CoreBundle\Entity\Instance;
 use Celsius3\CoreBundle\Entity\BaseUser;
+use Celsius3\CoreBundle\Entity\Instance;
 
 class OrderManager
 {
     public const TYPE__SEARCH = 'search';
     public const TYPE__PROVISION = 'provision';
 
-    public static function getTypes()
+    public static function getTypes(): array
     {
-        return array(
+        return [
             self::TYPE__SEARCH,
             self::TYPE__PROVISION,
-        );
+        ];
     }
 
-    public static function getTypeForUser(Instance $instance, BaseUser $user = null)
+    public static function getTypeForUser(Instance $instance, BaseUser $user = null): string
     {
-        if (!is_null($user)) {
-            return in_array($user->getBaseInstitution(), $instance->getOwnerInstitutions()->toArray()) ? self::TYPE__SEARCH : self::TYPE__PROVISION;
-        } else {
-            return self::TYPE__PROVISION;
+        if ($user !== null && $instance->getOwnerInstitutions()->contains($user->getBaseInstitution())) {
+            return self::TYPE__SEARCH;
         }
+
+        return self::TYPE__PROVISION;
     }
 }
