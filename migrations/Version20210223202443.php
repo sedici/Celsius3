190c889e115b6ac5065700d8c94fb92fbbf01b08
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2021-02-24 15:13:54 -0300

    Correción de migración

--- migrations/Version20210223202443.php
+++ migrations/Version20210223202443.php
@@ -18,6 +18,7 @@ final class Version20210223202443 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE data_request ADD type VARCHAR(255) NOT NULL');
         $this->addSql('CREATE TABLE orders_data_request (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, data LONGTEXT NOT NULL, exported TINYINT(1) NOT NULL, downloaded TINYINT(1) NOT NULL, visible TINYINT(1) NOT NULL, file VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users_data_request (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, data LONGTEXT NOT NULL, exported TINYINT(1) NOT NULL, downloaded TINYINT(1) NOT NULL, visible TINYINT(1) NOT NULL, file VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
@@ -26,6 +27,7 @@ final class Version20210223202443 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE data_request DROP type');
         $this->addSql('DROP TABLE orders_data_request');
         $this->addSql('DROP TABLE users_data_request');
     }
