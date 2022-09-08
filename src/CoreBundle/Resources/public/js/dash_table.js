ff8aff20e38f287fe78d9479912e2715ff35555e
Sobrado Ariel <asobrado@gmail.com>
2022-09-08 11:29:26 -0300

    fix para que funcione listado de instancias

--- src/CoreBundle/Resources/public/js/dash_table.js
+++ src/CoreBundle/Resources/public/js/dash_table.js
@@ -1,17 +1,29 @@
 var $ = require('jquery');
 
-$('#orderuser-accordion').one('show.bs.collapse', function () {
+$('#orderuser-accordion').one('click', function () {
     'use strict';
+
     $.ajax({
         type: 'get',
         format: 'json',
         url: Routing.generate('superadmin_orderusertable'),
-        success: function (data) {
+        success: function success(data) {
             data = JSON.parse(data);
             if (data.length > 0) {
                 $('#orderuser-table > tbody').empty();
                 $(data).each(function (i, elem) {
-                    $('#orderuser-table > tbody').append(ich.orderuser(elem));
+                    var name = elem.name ? elem.name : '';
+                    var provisionOrders = elem.provisionOrders ? elem.provisionOrders : '';
+                    var pendingUsers = elem.pendingUsers ? elem.pendingUsers : '';
+                    var searchedOrders = elem.searchedOrders ? elem.searchedOrders : '';
+                    var requestedOrders = elem.requestedOrders ? elem.requestedOrders : '';
+                    var satisfiedOrders = elem.satisfiedOrders ? elem.satisfiedOrders : '';
+                    var totalUsers = elem.totalUsers ? elem.totalUsers : '';
+
+                    var totalOrders = elem.totalOrders ? elem.totalOrders : '';
+
+                    var html = '<tr><td>' + name + '</td><td>' + elem.country + '</td><td>' + totalOrders + '</td><td>' + provisionOrders + '</td><td></td><td>' + searchedOrders + '</td><td>' + requestedOrders + '</td><td>' + satisfiedOrders + '</td><td>' + pendingUsers + '</td><td>' + totalUsers + '</td></tr>';
+                    $('#orderuser-table > tbody').append(html);
                 });
             }
             $('#orderuser-table').dataTable({
