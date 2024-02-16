e8eab1b97e42417f34c91b744ef36b3e64670d39
Sobrado Ariel <asobrado@gmail.com>
2024-02-16 08:26:07 -0300

    se elimino la lib de angular route

--- assets/ng/Administration/resources.js
+++ assets/ng/Administration/resources.js
@@ -1,4 +1,4 @@
-var administrationApp = angular.module('administrationApp', [require('angular-route')]);
+var administrationApp = angular.module('administrationApp');
 administrationApp.factory('Order', ['$resource', function ($resource) {
     return $resource(Routing.generate('admin_rest_order') + ':id',
         {id: '@id'}, {
